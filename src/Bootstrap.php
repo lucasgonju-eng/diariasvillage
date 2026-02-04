df0d799e2d76b8955860508971edda38571bc70d
Lucas Gonçalves Júnior <lucasgonju@gmail.com>
2026-02-03 21:16:02 -0300

    chore: registrar erros em arquivo local

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,6 +6,9 @@ use App\Env;
 
 Env::load(dirname(__DIR__));
 
+ini_set('log_errors', '1');
+ini_set('error_log', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'error_log_custom.txt');
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
